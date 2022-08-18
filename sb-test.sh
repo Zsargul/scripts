b7f2a599e3520a7747bccfec883525a97e40e07b
Zsargul <kubakerlino@gmail.com>
2022-08-18 22:35:35 +0100

    remove 3 test scripts

--- sb-test.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-echo "hello world"
-
-
-case $BUTTON in
-	1) st -e "$EDITOR" "$0" ;;
-esac
--- sb-test3.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-echo "hello world 3 😄"
-
-
-case $BUTTON in
-	1) st -e "$EDITOR" "$0" ;;
-esac
